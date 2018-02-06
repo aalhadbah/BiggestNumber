9e6a2e2c23370ebc92b78d9d4fc8eb573552aaba
aalhadbah <aalhadbah@mail.csuchico.edu>
2018-02-05 18:31:46 -0800

    initial commit

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,16 +1,24 @@
-//Author:
+//Author:abdulaziz alhadbah
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int first,second;  //declaring whole number
+
+
   cout<<"Please enter a whole number:\n";
+cin>> first; 
+   //storing variables
+
   
   cout<<"Please enter another whole number:\n";
+cin>> second;
+    //storing variables
 
-  cout<<"Of those two numbers, the biggest is: ";
+  //cout<<"Of those two numbers, the biggest is: ";
+  
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
